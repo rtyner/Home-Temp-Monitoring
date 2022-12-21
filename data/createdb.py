98b2a289f21f774dade34fe97cf7105c76b99418
rt <rusty@rtyner.com>
2022-12-21 03:00:41 +0000

    updated dns records

--- data/createdb.py
+++ data/createdb.py
@@ -4,15 +4,9 @@ import sqlite3
 
 # Connecting to sqlite
 # connection object
-<<<<<<< HEAD:data/createdb.py
-<<<<<<< HEAD:db/createdb.py
 connection_obj = sqlite3.connect('../data/database.db')
-=======
 connection_obj = sqlite3.connect('database.db')
->>>>>>> c5e9a625483a89ae0e70a774f97cfd1823a8e991:data/createdb.py
-=======
 connection_obj = sqlite3.connect('../data/database.db')
->>>>>>> 377375c74b14d59b18ecc1a3795ce74207d54b52:db/createdb.py
  
 # cursor object
 cursor_obj = connection_obj.cursor()
